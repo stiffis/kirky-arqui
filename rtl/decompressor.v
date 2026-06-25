15b3f85f8ebb68ec3575a593accf32c521f88c0c
stiffis <steve.ildefonso@utec.edu.pe>
2026-06-24 23:24:02 -0500

    refactor(decompressor): nombres de campos (op, funct3, rd, immci)

--- rtl/decompressor.v
+++ rtl/decompressor.v
@@ -2,16 +2,20 @@ module decompressor(input  [31:0] instrraw,
                     output reg [31:0] instr,
                     output        compressed);
 
-  wire [15:0] c = instrraw[15:0];
+  wire [15:0] c      = instrraw[15:0];
+  wire [1:0]  op     = c[1:0];
+  wire [2:0]  funct3 = c[15:13];
+  wire [4:0]  rd     = c[11:7];
+  wire [11:0] immci  = {{7{c[12]}}, c[6:2]};
 
-  assign compressed = (instrraw[1:0] != 2'b11);
+  assign compressed = (op != 2'b11);
 
   always @* begin
     instr = instrraw;
     if (compressed) begin
-      case ({c[1:0], c[15:13]})
-        5'b01000: instr = {{7{c[12]}}, c[6:2], c[11:7], 3'b000, c[11:7], 7'b0010011};
-        default:  instr = instrraw;
+      case ({op, funct3})
+        5'b01_000: instr = {immci, rd, 3'b000, rd, 7'b0010011};
+        default:   instr = instrraw;
       endcase
     end
   end
